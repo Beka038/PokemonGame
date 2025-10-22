9bb1d39207d6e81b95c31a65fd7ebe56b924c171
admin <beksultanerkinbai@gmail.com>
2025-10-22 20:19:17 +0500

    Second Change

--- logic.py
+++ logic.py
@@ -13,6 +13,9 @@ class Pokemon:
         self.shiny = self.shiny_pokemon()
         self.img = self.get_img()
         
+        self.hp = randint(75, 101)
+        self.power = randint(15,30)
+        
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения картинки покемона через API
@@ -43,17 +46,53 @@ class Pokemon:
             return (data['forms'][0]['name'])
         else:
             return "Pika Pika Pika Chu Shuchu Ne rabotaet"
-
+        
+        
+    def attack(self, enemy):
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            self.hp -= enemy.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
 
     # Метод класса для получения информации
     def info(self):
         return f'''
             Имя твоего покеомона: {self.name}
-            '''
+            Здоровья: {self.hp}
+            Урон: {self.power}
+            
+            '''#free place for class of the pokemon Fighter or Wizard
 
-    # Метод класса для получения картинки покемона
-    def show_img(self):
-        return self.img
 
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-атаку силой:{super_power}"
+    def info(self):
+        info_and_pokemon_type = super().info(self) + 'Вид покемона: Wizard  '
+        return f'''
+            Твой покемон: {info_and_pokemon_type}
+    '''
 
+class Wizard(Pokemon):
+    def attack(self, enemy):
+        chance = randint(0,4)
+        if chance==1:
+            shield = randint(10, 20)
+            self.hp += shield
+            return super().attack(enemy) + f"\nБоец использовал щит на: {shield}"
+        else:
+            return super().attack(enemy)
+            
+    def info(self):
+        info_and_pokemon_type = super().info(self) + 'Вид покемона: Wizard  '
+        return f'''
+            Твой покемон: {info_and_pokemon_type}
+    '''
 
--- main.py
+++ main.py
@@ -1,19 +1,41 @@
-import telebot 
+import telebot
+from random import randint 
 from config import token
 
-from logic import Pokemon
+from logic import Pokemon, Fighter, Wizard
 
 bot = telebot.TeleBot(token) 
 
 @bot.message_handler(commands=['go'])
 def go(message):
     #if message.from_user.username not in Pokemon.pokemons.keys():
-        pokemon = Pokemon(message.from_user.username)
+        chance = randint(0,11)
+        if chance == 1:
+            pokemon = Fighter(message.from_user.username)
+            bot.send_message(message.chat.id, f"Ваш покемон FIGHTER 💪")
+        elif chance == 2:
+            pokemon = Wizard(message.from_user.username)
+            bot.send_message(message.chat.id, f"Ваш покемон WIZARD 💥")
+        else:
+            pokemon = Pokemon(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
     #else:
         #bot.reply_to(message, "Ты уже создал себе покемона")
 
+@bot.message_handler(commands=['attack'])
+def fight(message):
+    if message.reply_to_message:
+        if message.reply_to_message.from_user.username in Pokemon.pokemons.keys() and message.from_user.username in Pokemon.pokemons.keys():
+            enemy = Pokemon.pokemons[message.reply_to_message.from_user.username]
+            pok = Pokemon.pokemons[message.from_user.username]
+            res = pok.attack(enemy)
+            bot.send_message(message.chat.id, res)
+        else:
+            bot.send_message(message.chat.id, "Сражатся можно только с покемонами")
+    else:
+        bot.send_message(message.chat.id, "Чтобы сражатся ответте на сообщение другого пользователя")
+    
 
 bot.infinity_polling(none_stop=True)
 
